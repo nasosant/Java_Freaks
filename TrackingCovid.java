a7ea126d9f92f7f1f171f792f902de28249cc646
nmermigas <nikosmermigas01@gmail.com>
2020-11-11 12:33:02 +0200

    Changed if block, added switch instead

--- TrackingCovid.java
+++ TrackingCovid.java
@@ -4,35 +4,41 @@ import java.util.Scanner;
 
 public class TrackingCovid19 {
 
-private static int suspiciousCases = 0;
-private static int allHealthyPeople = 10430106;
-private static int afterHealthyPeople = 0;
-private static int beforeHealthyPeople = 0;
+    private static int suspiciousCases = 0;
+    private static int allHealthyPeople = 10430106;
+    private static int afterHealthyPeople = 0;
+    private static int beforeHealthyPeople = 0;
 
-public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-    printMenu();
-    int answer = input.nextInt();
-    if (answer == 1) {
-    	Patients.addPatient();
-    } else if (answer == 2) {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        printMenu();
+        int answer = input.nextInt();
 
-    } else if (answer ==3) {
-
-    } else if (answer == 4) {
-
-    } else {
-
+        switch (answer) {
+            case 1:
+                Patients.addPatient();
+                break;
+            case 2:
+                //add a suspicious case-method
+                break;
+            case 3:
+                //Move someone from a patient to a healthy person, if he/she has get the virus over-method
+                break;
+            case 4:
+                //Say if a suspicious case is positive or negative to the testmethod
+                break;
+            case 5:
+                //Show all the results until now
+                break;
+        }
     }
 
-}
-
-public static void printMenu() {
-    System.out.println("1. Add a patient.");
-    System.out.println("2. Add a suspicious case.");
-    System.out.println("3. Move someone from a patient to a healthy person, if he/she has get the virus over.");
-    System.out.println("4. Say if a suspicious case is positive or negative to the test.");
-    System.out.println("5. Show all the results until now.");
-    System.out.print("Choose an option: ");
-}
+    public static void printMenu() {
+        System.out.println("1. Add a patient.");
+        System.out.println("2. Add a suspicious case.");
+        System.out.println("3. Move someone from a patient to a healthy person, if he/she has get the virus over.");
+        System.out.println("4. Say if a suspicious case is positive or negative to the test.");
+        System.out.println("5. Show all the results until now.");
+        System.out.print("Choose an option: ");
+    }
 }
