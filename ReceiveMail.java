4aa4b9307eb3f9da9f109001d02b5b2ff32467d8
aristotelis gkithkopoulos <aristogithk@gmail.com>
2020-11-13 03:16:17 +0200

    try to fix access gmail 1 error to discuss

--- ReceiveMail.java
+++ ReceiveMail.java
@@ -1,55 +1,38 @@
-import com.sun.mail.pop3.POP3Store;
-
-import javax.mail.*;
-import java.io.IOException;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Store;
 import java.util.Properties;
 
-public class ReceiveMail{
+public class ReceiveMail {
 
-    public static void receiveEmail(String pop3Host, String storeType,
-                                    String user, String password) {
+    public static void main(String args[]) throws Exception {
         try {
             //1) get the session object
-            Properties properties = new Properties();
-            properties.put("mail.pop3.host", pop3Host);
-            Session emailSession = Session.getDefaultInstance(properties);
+            Properties properties = System.getProperties();
+            properties.setProperty("mai.store.protocol", "imaps");
 
-            //2) create the POP3 store object and connect with the pop server
-            POP3Store emailStore = (POP3Store) emailSession.getStore(storeType);
-            emailStore.connect(pop3Host,user,password);
+            Session emailSession = Session.getDefaultInstance(properties, null);
 
-            //3) create the folder object and open it
-            Folder emailFolder = emailStore.getFolder("INBOX");
-            emailFolder.open(Folder.READ_ONLY);
+            //2) connect with the store
+            Store store = emailSession.getStore("imaps");
+            store.connect("imap.gmail.com","javafreaks.dmst@gmail.com","javafreaks");
 
-            //4) retrieve the messages from the folder in an array and print it
-            Message[] messages = emailFolder.getMessages();
-            for (int i = 0; i < messages.length; i++) {
-                Message message = messages[i];
-                System.out.println("---------------------------------");
-                System.out.println("Email Number " + (i + 1));
-                System.out.println("Subject: " + message.getSubject());
-                System.out.println("From: " + message.getFrom()[0]);
-                System.out.println("Text: " + message.getContent().toString());
+            //3) create folder??
+            Folder inbox = store.getFolder("inbox");
+            inbox.open(Folder.READ_ONLY);
+
+            //4)find the message
+            Message messages [] = inbox.getMessages();
+            for (Message message : messages){
+                System.out.println(message.getSubject());
             }
 
-            //5) close the store and folder objects
-            emailFolder.close(false);
-            emailStore.close();
 
-        } catch (NoSuchProviderException e) {e.printStackTrace();}
-        catch (MessagingException e) {e.printStackTrace();}
-        catch (IOException e) {e.printStackTrace();}
-    }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
-    public static void main(String[] args) {
-
-        String host = "imap.gmail.com";//change accordingly
-        String mailStoreType = "pop3";
-        String username= "javafreaks.dmst@gmail.com";
-        String password= "javafreaks";//change accordingly
-
-        receiveEmail(host, mailStoreType, username, password);
 
     }
 }
