c7b2299059b004c5e7b3b6805c7aa70ea037f15b
Grigoris Ntrazios <t8190124@aueb.gr>
2020-11-13 23:23:45 +0200

    First commit of VictimContact

--- VictimContact.java
+++ VictimContact.java
@@ -1,3 +1,33 @@
-public class VictimContact extends AllPeople{
+import java.util.Scanner;
+
+public class VictimContact extends AllPeople {
+	
+	public VictimContact(String name, String surname) {
+		super(name, surname);
+		// oti allh plhroforia zhtao na dinetai kata thn epikoinonia 
+	}
+	
+	
+	public static void addVictimContact() {
+		 Scanner input = new Scanner(System.in);
+		 System.out.print("Enter the Name of the patient: ");
+		 String name = input.next();
+		 System.out.print("Enter the surname: ");
+		 String surname = input.next();
+		 VictimContact possiblepatient = new VictimContact(name, surname);
+		 input.close();
+	}
+
+	
+	public void classifyContact () {
+		//diaxorismos upsilou-xamhlou kindunou
+		String result;
+		if () {
+			result="υψηλού";
+		} else {
+			result="χαμηλού";
+		}
+	}
 
 }
+
