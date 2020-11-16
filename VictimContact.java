5b8bf6e731af6b8c38ad64d732ba842fd04a8b43
xenofon kaps <t8190057@aueb.gr>
2020-11-16 22:09:24 +0200

    modify class Victim

--- VictimContact.java
+++ VictimContact.java
@@ -2,30 +2,39 @@ import java.util.Scanner;
 
 public class VictimContact extends AllPeople {
 
-	protected static VictimContact victimContactTable[] = new VictimContact[100];
-	protected static VictimContact HighDanger[] = new VictimContact[100];
-	protected static VictimContact LowDanger[] = new VictimContact[100];
-	private static int counter = 0;
+	        protected static VictimContact victimContactTable[] = new VictimContact[100];
+	        protected static VictimContact HighDanger[] = new VictimContact[100];
+	        protected static VictimContact LowDanger[] = new VictimContact[100];
+	        private static int i=0;
+	        private static int j=0;
+	        private static int counter = 0;
+	        protected String housemate;
+	        protected String directContact;
+	        protected String contactwhilesymptomsoccured;
+	        protected String placeToMeet;
+	        protected double Distance;
+	        protected double Time;
+	        protected String Worksector;
+	        
+	        public VictimContact(String name, String surname, String address, String email, int phonenumber, int AMKA, 
+	        						String housemate, String directContact, String contactwhilesymptomsoccured,
+	        						double Distance, String placeToMeet, double time, String Worksector) {
+	                super(name, surname, address, email, phonenumber, AMKA);
+	                this.housemate = housemate;
+	                this.directContact = directContact;
+	                this.contactwhilesymptomsoccured = contactwhilesymptomsoccured;
+	                this.placeToMeet = placeToMeet;
+	                this.Distance = Distance;
+	                this.Time = time;
+	                this.Worksector = Worksector;
+	                if (counter == victimContactTable.length) {
+	                        victimContactTable = (VictimContact[]) duplicateTable(victimContactTable.length, victimContactTable);
+	                }
+	                victimContactTable[counter] = this;
+	                counter++;
+	        }
 
-	public VictimContact(String name, String surname, String address, String email, int phonenumber, int AMKA) {
-		super(name, surname, address, email, phonenumber, AMKA);
-		// oti allh plhroforia zhtao na dinetai kata thn epikoinonia
-		if (counter == victimContactTable.length) {
-			victimContactTable = duplicateTable(victimContactTable.length, victimContactTable);
-		}
-		victimContactTable[counter] = this;
-		counter++;
-	}
-
-	public static VictimContact[] duplicateTable(int length, VictimContact[] victimContactTable) {
-		VictimContact newvictimContactTable[] = new VictimContact[2 * length];
-		for (int i = 0; i < length; i++) {
-			newvictimContactTable[i] = victimContactTable[i];
-		}
-		return newvictimContactTable;
-	}
-
-	public static void addVictimContact() {
+		public static void addVictimContact() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter the patient's personal details");
 		System.out.print("Enter the name: ");
@@ -40,19 +49,37 @@ public class VictimContact extends AllPeople {
 		int phonenumber = input.nextInt();
 		System.out.print("Enter the AMKA: ");
 		int AMKA = input.nextInt();
-		possiblepatient = new VictimContact(name, surname, address, email, phonenumber, AMKA);
+		possiblepatient = new VictimContact(name, surname, address,
+				email, phonenumber, AMKA);
 	}
+		    public void highdanger() {
+	    	
+	    	System.out.println("The contact should abide by the following orders.");
+	    	System.out.println("Firstly, staying indoors and have health supervision.");
+	    	System.out.println("Secondly,.");
+	    	System.out.println(".");
+	    	System.out.println(".");
+	    	
+	    }
 
-	// diaxorismos upsilou-xamhlou kindunou
+	    public void lowdanger() {
+	    	
+	    }
+	    
+        public void classifyContact(String Housemate, String DirectContact, String Contactwhilesymptomsoccured,
+				double distance, String PlaceToMeet, double Time, String worksector) {
 
-	/*public void classifyContact() {
-		String result;
-		if (true) {
-			result = "υψηλού";
-		} else {
-			result = "χαμηλού";
-		}
-	}*/
-
+                if (Housemate == "yes" || DirectContact == "yes" || Contactwhilesymptomsoccured == "yes"
+                		|| (distance <= 2 && Time > 15) || PlaceToMeet == "internal" || worksector == "healthdomain") {
+                	 HighDanger[i] = victimContactTable[counter];
+                     i++;
+                     highdanger();
+                } else {
+                	 LowDanger[j] = victimContactTable[counter];   
+                	 j++;
+                	 lowdanger();
+                }
+                
+        }
 }
 
