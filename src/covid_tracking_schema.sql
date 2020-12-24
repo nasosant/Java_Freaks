491ab0d36feceaa94ef06821fc408fdf3960a51e
Grigoris Ntrazios <t8190124@aueb.gr>
2020-12-24 03:44:58 +0200

    Changes in the creation of tables

--- src/covid_tracking_schema.sql
+++ src/covid_tracking_schema.sql
@@ -1,27 +1,27 @@
-CREATE database covid_tracking;
-
+-- CREATE database covid_tracking;
+-- DROP DATABASE covid_tracking;
 USE covid_tracking;
-DROP DATABASE covid_tracking;
-
-/* ΚΛΑΣΗ ΜΕ ΤΑ ΚΟΙΝΑ ΣΤΟΙΧΕΙΑ ΤΩΝ confirmed ΚΑΙ victim_contacts ΟΙ ΟΠΟΙΕΣ ΤΩΝ ΚΑΝΟΥΝ EXTEND
-
-CREATE TABLE AllPeople (
-	id int PRIMARY KEY auto_increment,
-	surname VARCHAR(20) NOT NULL,
-	AMKA INT NOT NULL,
-	area VARCHAR(20) NOT NULL,
-	street VARCHAR(50) NOT NULL,
-	street_number INT NOT NULL,
-	zip INT NOT NULL,
-	email VARCHAR(20)
-);
+/*
+DROP TABLE Person;
+DROP TABLE low_danger;
+DROP TABLE high_danger;
+DROP TABLE contact_phone;
+DROP TABLE victim_contacts;
+DROP TABLE deceased;
+DROP TABLE cured;
+DROP TABLE confirmed_phone;
+DROP TABLE confirmed;
 */
+CREATE TABLE Person (
+	id INT PRIMARY KEY,
+	name VARCHAR(20)
+);
 
 CREATE TABLE confirmed(
-	confirmed_name VARCHAR(20) NOT NULL,  -- ΜΗΠΩΣ ΔΕΝ ΧΡΕΙΑΖΕΤΑΙ??
-	surname VARCHAR(20) NOT NULL,
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	AMKA INT NOT NULL,
+	name VARCHAR(20) NOT NULL,
+	surname VARCHAR(20) NOT NULL,
+	AMKA VARCHAR(9) UNIQUE,
 	area VARCHAR(20) NOT NULL,
 	street VARCHAR(50) NOT NULL,
 	street_number INT NOT NULL,
@@ -34,46 +34,57 @@ CREATE TABLE confirmed(
 	id INT PRIMARY KEY,
 	phone VARCHAR(20) NOT NULL,
 	CONSTRAINT FK_confirmed_phone_id FOREIGN KEY (id) REFERENCES confirmed (id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
  
 CREATE TABLE cured(
 	id INT PRIMARY KEY,
 	cured_date DATE NOT NULL,
 	CONSTRAINT FK_cured_id FOREIGN KEY (id) REFERENCES confirmed (id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
  );
 
 CREATE TABLE deceased(
 	id INT PRIMARY KEY,
 	deceased_date DATE NOT NULL,
 	CONSTRAINT FK_deceased_id FOREIGN KEY (id) REFERENCES confirmed (id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE victim_contacts(
-	surname VARCHAR(20) NOT NULL,
 	contact_id INT PRIMARY KEY AUTO_INCREMENT,
-	confirmed_id INT,      -- ΜΗΠΩΣ ΘΕΛΕΙ NOT NULL
-	AMKA INT NOT NULL,
-	area VARCHAR(20) NOT NULL,
-	street VARCHAR(50) NOT NULL,
-	street_number INT NOT NULL,
-	zip INT NOT NULL,
+	name VARCHAR(30) NOT NULL,
+	surname VARCHAR(20) NOT NULL,
+	confirmed_id INT NOT NULL,
+	AMKA INT NOT NULL, -- Το πιο πιθανό είναι να μην το ξέρει
 	email VARCHAR(50),
 	victim_relationship VARCHAR(20) NOT NULL,
 	CONSTRAINT FK_victim_contacts_confirmed_id FOREIGN KEY (confirmed_id) REFERENCES confirmed (id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE contact_phone(
 	id INT PRIMARY KEY,
 	phone VARCHAR(20) NOT NULL,
 	CONSTRAINT FK_contact_phone_id FOREIGN KEY (id) REFERENCES victim_contacts (contact_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
  
- CREATE TABLE high_danger(
+CREATE TABLE high_danger(
 	id INT PRIMARY KEY,
 	CONSTRAINT FK_high_danger_id FOREIGN KEY (id) REFERENCES victim_contacts (contact_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
  
- CREATE TABLE low_danger(
+CREATE TABLE low_danger(
 	id INT PRIMARY KEY,
 	CONSTRAINT FK_low_danger_id FOREIGN KEY (id) REFERENCES victim_contacts (contact_id)
- );
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
