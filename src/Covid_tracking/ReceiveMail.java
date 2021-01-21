62c86887971036248989a1da417b51f4bd69dddf
Grigoris Ntrazios <t8190124@aueb.gr>
2021-01-21 23:07:35 +0200

    o githk einai gay

--- src/Covid_tracking/ReceiveMail.java
+++ src/Covid_tracking/ReceiveMail.java
@@ -12,157 +12,155 @@ import java.util.Scanner;
 
 public class ReceiveMail {
 
-    public static String[][] VicContacts = new String[100][3];
-    public static int VicCount = 0;
-    public static Store store;
+	public static String[][] VicContacts = new String[100][3];
+	public static int VicCount = 0;
+	public static Store store;
+	public static String name;
+	public static String surname;
+	public static String email;
+	public static int phonenumber;
+	public static int AMKA;
+	public static int confirmed_id;
+	public static String victim_relationship;
+	public static String danger;
 
-    public static void connect() throws Exception{
-        //1) get the session object
-        Properties properties = System.getProperties();
-        properties.setProperty("mail.store.protocol", "imaps");
+	public static void connect() throws Exception {
+		// 1) get the session object
+		Properties properties = System.getProperties();
+		properties.setProperty("mail.store.protocol", "imaps");
 
-        Session emailSession = Session.getDefaultInstance(properties, null);
+		Session emailSession = Session.getDefaultInstance(properties, null);
 
-        //2) connect with the store
-        store = emailSession.getStore("imaps");
-        store.connect("imap.gmail.com", "javafreaks.dmst@gmail.com", "javafreaks");
+		// 2) connect with the store
+		store = emailSession.getStore("imaps");
+		store.connect("imap.gmail.com", "javafreaks.dmst@gmail.com", "javafreaks");
 
-    }
+	}
 
+	public static void receiveMail() throws Exception {
 
-    public static void receiveMail() throws Exception {
+		connect();
 
-        connect();
+		// 3) create folder??
+		Folder emailFolder = store.getFolder("INBOX");
+		emailFolder.open(Folder.READ_ONLY);
 
-        //3) create folder??
-        Folder emailFolder = store.getFolder("INBOX");
-        emailFolder.open(Folder.READ_ONLY);
+		// 4)find the message
+		Message[] messages = emailFolder.getMessages();
 
-        //4)find the message
-        Message[] messages = emailFolder.getMessages();
+		for (Message message : messages) {
 
-        for (Message message : messages) {
+			// System.out.println(message.getContent());
+			Object mess = message.getContent();
+			Scanner sc = new Scanner(mess.toString());
+			int c = 0;
+			EmailToWords(message);
+			c = c + 1;
 
-            // System.out.println(message.getContent());
-            Object mess = message.getContent();
-            Scanner sc = new Scanner(mess.toString());
-            int c = 0;
-            EmailToWords(message);
-            c = c + 1;
+		}
+		email();
 
-        }
-        email();
+	}
 
-    }
+	public static void EmailToWords(Message message) throws IOException, MessagingException {
 
-    public static void EmailToWords(Message message) throws IOException, MessagingException {
+		try {
+			if (message.getSubject().equals("Covid-19")) {
 
-        try {
-            if (message.getSubject().equals("Covid-19")) {
+				Object mess = message.getContent();
+				Scanner sc = new Scanner(mess.toString());
 
+				int wordCount = 0;
 
-                Object mess = message.getContent();
-                Scanner sc = new Scanner(mess.toString());
+				boolean word = false;
+				int endOfLine = mess.toString().length() - 1;
 
+				for (int i = 0; i < mess.toString().length(); i++) {
+					// if the char is a letter, word = true.
+					if (Character.isLetter(mess.toString().charAt(i)) && i != endOfLine) {
+						word = true;
+						// if char isn't a letter and there have been letters before,
+						// counter goes up.
+					} else if (!Character.isLetter(mess.toString().charAt(i)) && word) {
+						wordCount++;
+						word = false;
+						// last word of String; if it doesn't end with a non letter, it
+						// wouldn't count without this.
+					} else if (Character.isLetter(mess.toString().charAt(i)) && i == endOfLine) {
+						wordCount++;
+					}
+				}
 
-                int wordCount = 0;
+				for (int i = 1; i < wordCount; i++) {
+					try {
+						String helper = sc.next();
 
-                boolean word = false;
-                int endOfLine = mess.toString().length() - 1;
+						AnalyseW(helper, i);
 
-                for (int i = 0; i < mess.toString().length(); i++) {
-                    // if the char is a letter, word = true.
-                    if (Character.isLetter(mess.toString().charAt(i)) && i != endOfLine) {
-                        word = true;
-                        // if char isn't a letter and there have been letters before,
-                        // counter goes up.
-                    } else if (!Character.isLetter(mess.toString().charAt(i)) && word) {
-                        wordCount++;
-                        word = false;
-                        // last word of String; if it doesn't end with a non letter, it
-                        // wouldn't count without this.
-                    } else if (Character.isLetter(mess.toString().charAt(i)) && i == endOfLine) {
-                        wordCount++;
-                    }
-                }
+					} catch (Exception e) {
 
-                for (int i = 1; i < wordCount; i++) {
-                    try {
-                        String helper = sc.next();
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 
-                        AnalyseW(helper, i);
+		}
+	}
 
-                    } catch (Exception e) {
+	public static void AnalyseW(String world, int a) {
+		switch (a) {
+		case (1):
+			if (world.equals("H")) {
+				danger = 'High'
+			}
 
-                    }
-                }
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+			if (world.equals("L")) {
+				VicContacts[VicCount][0] = "L";
 
-        }
-    }
+			}
+			break;
+		case (3):
+			VicContacts[VicCount][2] = world;
+			VicCount = VicCount + 1;
+			break;
+		case (2):
+			VicContacts[VicCount][1] = world;
+			break;
+		}
 
-    public static void AnalyseW(String world, int a) {
-        if (a > 3) {
-            a = a % 3;
-        }
-        switch (a) {
-            case (0):
-                VicContacts[VicCount][2] = world;
-            case (1):
-                if (world.equals("H")) {
-                    VicContacts[VicCount][0] = "H";
+	}
 
-                }
+	public static void email() {
 
-                if (world.equals("L")) {
-                    VicContacts[VicCount][0] = "L";
+		System.out.println(VicCount);
+		for (int i = 0; i < VicCount; i++) {
+			try {
+				if (VicContacts[i][0].equals("L")) {
+					Communication.sendMailtoLowDangerContacts(VicContacts[i][2]);
+				} else {
+					Communication.sendMailToHighDangerContacts(VicContacts[i][2]);
+				}
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
 
-                }
-                break;
-            case (3):
-                VicContacts[VicCount][2] = world;
-                VicCount = VicCount + 1;
-                break;
-            case (2):
-                VicContacts[VicCount][1] = world;
-                break;
-        }
+	}
 
+	public static void deleteMail() throws Exception {
 
-    }
+		connect();
+		// 3) create folder??
+		Folder emailFolder = store.getFolder("INBOX");
+		emailFolder.open(Folder.READ_ONLY);
 
-    public static void email() {
+		// 4)find the message
+		Message[] messages = emailFolder.getMessages();
 
-        System.out.println(VicCount);
-        for(int i=0 ; i < VicCount ; i++) {
-            try {
-                if (VicContacts[i][0].equals("L")) {
-                    Communication.sendMailtoLowDangerContacts(VicContacts[i][2]);
-                } else {
-                    Communication.sendMailToHighDangerContacts(VicContacts[i][2]);
-                }
-            }catch (Exception e){
-                System.out.println(e.getMessage());
-            }
-        }
+		for (Message message : messages) {
+			message.setFlag(Flags.Flag.DELETED, true);
+		}
 
-    }
-
-    public static void deleteMail() throws Exception {
-
-        connect();
-        //3) create folder??
-        Folder emailFolder = store.getFolder("INBOX");
-        emailFolder.open(Folder.READ_ONLY);
-
-        //4)find the message
-        Message[] messages = emailFolder.getMessages();
-
-        for (Message message : messages) {
-            message.setFlag(Flags.Flag.DELETED , true);
-        }
-
-    }
+	}
 }
